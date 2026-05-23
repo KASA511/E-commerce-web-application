ab1f984a2fcecc1edb90de8b3e4ca219600994eb
KASA511 <244g1a05c6@srit.ac.in>
2026-05-23 17:47:04 +0530

    Update orders.js

--- orders.js
+++ orders.js
@@ -1,7 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Orders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const res = await axios.get(
+          "http://localhost:5000/api/orders",
+          {
+            headers: {
+              authorization: token
+            }
+          }
+        );
+
+        setOrders(res.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
+
+    fetchOrders();
+  }, []);
+
   return (
     <div>
-      <h1>Orders Page</h1>
+      <h1>Orders</h1>
+
+      {orders.map((order) => (
+        <div key={order._id}>
+          <p>Total: ₹{order.totalAmount}</p>
+          <p>Status: {order.status}</p>
+        </div>
+      ))}
     </div>
   );
 }
