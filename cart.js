e98ac3a456082a4eef2a81a621926247e0dd4fef
KASA511 <244g1a05c6@srit.ac.in>
2026-05-23 17:47:37 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,7 +1,46 @@
+import axios from "axios";
+
 function Cart() {
+
+  const placeOrder = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const orderData = {
+        products: [
+          {
+            name: "Laptop",
+            quantity: 1
+          }
+        ],
+        totalAmount: 50000
+      };
+
+      const res = await axios.post(
+        "http://localhost:5000/api/orders",
+        orderData,
+        {
+          headers: {
+            authorization: token
+          }
+        }
+      );
+
+      alert("Order Placed");
+      console.log(res.data);
+
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
   return (
     <div>
       <h1>Cart Page</h1>
+
+      <button onClick={placeOrder}>
+        Checkout
+      </button>
     </div>
   );
 }
